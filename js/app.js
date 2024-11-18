70a2be8b4f40d1e42735f633609f97de2fdad01c
Azizbek Begmurodovich Sayfiyev <azizbekofficialaccaunt@gmail.com>
2024-11-18 16:03:16 +0500

    modal

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 function toggleMenu(button) {
   button.classList.toggle("active");
 }
+
+// catalog-modal
+
+// Elementlarni olish
+const menuBtn = document.querySelector(".menu-btn");
+const catalogModal = document.querySelector(".catolog-modal");
+
+// Tugma bosilganda classni toggle qilish
+menuBtn.addEventListener("click", () => {
+  catalogModal.classList.toggle("catalog-modal-active");
+});
